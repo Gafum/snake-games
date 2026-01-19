fbabf2181b5b828eba34b074a3a100269e5cfee8
Marchuk Dmytro <marcukdyma@gmail.com>
2026-01-19 13:45:55 +0100

    added moving

--- web/app.js
+++ web/app.js
@@ -35,6 +35,40 @@ function removeTail() {
    area = area.map(value => value > 0 ? value - 1 : 0);
 }
 
+function go() {
+   var indexOfMaxValue = area.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0); //head of the snake
+   if (direction == 'right') {
+      var newIndex = indexOfMaxValue + 1;
+   } else if (direction == 'left') {
+      var newIndex = indexOfMaxValue - 1;
+   } else if (direction == 'up') {
+      var newIndex = indexOfMaxValue - areaWidth;
+   } else if (direction == 'down') {
+      var newIndex = indexOfMaxValue + areaWidth;
+   }
+
+   area[newIndex] = Math.max(...area) + 1; //move head
+   removeTail(); //remove tail
+}
+
+// Input
+document.addEventListener('keydown', (e) => {
+   switch (e.key) {
+      case 'ArrowUp':
+         direction = 'up';
+         break;
+      case 'ArrowDown':
+         direction = 'down';
+         break;
+      case 'ArrowLeft':
+         direction = 'left';
+         break;
+      case 'ArrowRight':
+         direction = 'right';
+         break;
+   }
+});
+
 
 // Processing
 start();
